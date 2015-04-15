14b4c83d10cd32dc8289173b9f4a7e82bb7a445b
John González <johngonzalezv@gmail.com>
2015-04-15 12:37:47 -0500

    Conexión entre la colección Posts y la plantilla 'postlist'

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,6 +1,6 @@
 if(Posts.find().count() === 0) {
   Posts.insert({
-    title: 'Introducing Telescope',
+    title: 'Introducing Telescope 2',
     url: 'http://sachagreif.com/introducing-telescope/'
   });
 
