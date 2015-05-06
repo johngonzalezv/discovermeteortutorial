70924069d5b837274f9d0135d84c0f1dd28381e3
John González <johngonzalezv@gmail.com>
2015-05-06 10:04:57 -0500

    Creado y enlazado un paquete básico.

---
+++ packages/errors/package.js
@@ -0,0 +1,26 @@
+Package.describe({
+  name: 'johngonzalez:errors',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'A pattern to display application errors to the user',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/johngonzalezv/discovermeteortutorial.git',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
+  api.addFiles(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
+  if (api.export) {
+    api.export('Errors');
+  }
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('johngonzalez:errors');
+  api.addFiles('errors-tests.js');
+});
