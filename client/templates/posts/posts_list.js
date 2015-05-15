801ec1ff0021d9a56d0bcaef2f787b847463ab3b
John González <johngonzalezv@gmail.com>
2015-05-15 17:46:27 -0500

    Hacer aparecer y desaparecer los posts añadidos y eliminados.

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,5 +1,11 @@
 Template.postsList.onRendered(function() {
   this.find('.wrapper')._uihooks = {
+    insertElement: function(node, next) {
+      $(node)
+        .hide()
+        .insertBefore(next)
+        .fadeIn();
+    },
     moveElement: function (node, next) {
       // node is the element intends move. next is the next of future position
       var $node = $(node), $next=$(next);
@@ -34,6 +40,11 @@ Template.postsList.onRendered(function() {
       // reset evething to 0, animated
       $(node).addClass('animate').css('top', 0);
       $inBetween.addClass('animate').css('top', 0);
+    },
+    removeElement: function(node) {
+      $(node).fadeOut(function() {
+        $(this).remove();
+      });
     }
   }
 });
